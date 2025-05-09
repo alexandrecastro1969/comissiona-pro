d92168882b7bc589940e55cf39ebacbb422f24b0
alexandrecastro1969 <alexandre.aguiarcastro@gmail.com>
2025-05-09 12:48:48 -0300

    feat: Ajuste do next.config.js removendo o contentSecurityPolicy.

--- next.config.js
+++ next.config.js
@@ -23,29 +23,28 @@ const nextConfig = {
     deviceSizes: [640, 750, 828, 1080, 1200],
     imageSizes: [16, 32, 48, 64, 96],
     dangerouslyAllowSVG: true,
-    contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
   },
 
   // Configurações de segurança
-  async headers() {
-    return [
-      {
-        source: '/:path*',
-        headers: [
-          {
-            key: 'Content-Security-Policy',
-            value: [
-              "default-src 'self'",
-              "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://vercel.live",
-              "style-src 'self' 'unsafe-inline'",
-              "img-src 'self' data: blob: https:",
-              "font-src 'self'"
-            ].join('; ')
-          }
-        ]
-      }
-    ]
-  }
+  // async headers() {
+  //   return [
+  //     {
+  //       source: '/:path*',
+  //       headers: [
+  //         {
+  //           key: 'Content-Security-Policy',
+  //           value: [
+  //             "default-src 'self'",
+  //             "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://vercel.live",
+  //             "style-src 'self' 'unsafe-inline'",
+  //             "img-src 'self' data: blob: https:",
+  //             "font-src 'self'"
+  //           ].join('; ')
+  //         }
+  //       ]
+  //     }
+  //   ]
+  // }
 }
 
 module.exports = withNextIntl(withMDX(nextConfig));
